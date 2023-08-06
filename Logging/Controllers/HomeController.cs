e3202f0dff2dfac7b51722b0e59a31a0df3400ca
Chris Carter <christopher.carter.777@gmail.com>
2023-08-05 22:50:36 -0700

    Basic debug logging. Boilerplate copied across projects.

--- Logging/Controllers/HomeController.cs
+++ Logging/Controllers/HomeController.cs
@@ -4,63 +4,61 @@ using Logging.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace Logging.Controllers
+namespace Logging.Controllers;
+public class HomeController : Controller
 {
-     public class HomeController : Controller
+     private readonly ILogger<HomeController> _logger;
+     private readonly ITranscriptBuilder _transcriptBuilder;
+
+     public HomeController(ILogger<HomeController> logger, ITranscriptBuilder transcriptBuilder)
      {
-          private readonly ILogger<HomeController> _logger;
-          private readonly ITranscriptBuilder _transcriptBuilder;
+          _logger = logger;
+          _transcriptBuilder = transcriptBuilder;
+     }
 
-          public HomeController(ILogger<HomeController> logger, ITranscriptBuilder transcriptBuilder)
+     public IActionResult Index()
+     {
+          Guid session = Guid.NewGuid();
+          _logger.LogInformation($"{nameof(Index)} session {session} starting.");
+          try
           {
-               _logger = logger;
-               _transcriptBuilder = transcriptBuilder;
+               List<Transcript> transcripts = _transcriptBuilder.BuildMany(session, 3, false);
+               TranscriptsViewModel viewModel = new TranscriptsViewModel(transcripts);
+               return View(viewModel);
           }
+          catch (Exception ex)
+          {
+               _logger.LogError(ex, $"{nameof(Index)} session {session}");
+               TranscriptsViewModel viewModel = new TranscriptsViewModel(ex.Message);
+               return View(viewModel);
+          }
+     }
+     public IActionResult ErroredIndex()
+     {
+          Guid session = Guid.NewGuid();
+          _logger.LogInformation($"{nameof(ErroredIndex)} session {session} starting.");
+          try
+          {
+               List<Transcript> transcripts = _transcriptBuilder.BuildMany(session, 3, true);
+               TranscriptsViewModel viewModel = new TranscriptsViewModel(transcripts);
+               return View("Index", viewModel);
+          }
+          catch (Exception ex)
+          {
+               _logger.LogError(ex, $"{nameof(ErroredIndex)} session {session}");
+               TranscriptsViewModel viewModel = new TranscriptsViewModel(ex.Message);
+               return View("Index", viewModel);
+          }
+     }
 
-          public IActionResult Index()
-          {
-               Guid session = Guid.NewGuid();
-               _logger.LogInformation($"{nameof(Index)} session {session} starting.");
-               try
-               {
-                    List<Transcript> transcripts = _transcriptBuilder.BuildMany(session, 3, false);
-                    TranscriptsViewModel viewModel = new TranscriptsViewModel(transcripts);
-                    return View(viewModel);
-               }
-               catch (Exception ex)
-               {
-                    _logger.LogError(ex, $"{nameof(Index)} session {session}");
-                    TranscriptsViewModel viewModel = new TranscriptsViewModel(ex.Message);
-                    return View(viewModel);
-               }
-          }
-          public IActionResult ErroredIndex()
-          {
-               Guid session = Guid.NewGuid();
-               _logger.LogInformation($"{nameof(ErroredIndex)} session {session} starting.");
-               try
-               {
-                    List<Transcript> transcripts = _transcriptBuilder.BuildMany(session, 3, true);
-                    TranscriptsViewModel viewModel = new TranscriptsViewModel(transcripts);
-                    return View("Index", viewModel);
-               }
-               catch (Exception ex)
-               {
-                    _logger.LogError(ex, $"{nameof(ErroredIndex)} session {session}");
-                    TranscriptsViewModel viewModel = new TranscriptsViewModel(ex.Message);
-                    return View("Index", viewModel);
-               }
-          }
+     public IActionResult Privacy()
+     {
+          return View();
+     }
 
-          public IActionResult Privacy()
-          {
-               return View();
-          }
-
-          [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-          public IActionResult Error()
-          {
-               return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-          }
+     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+     public IActionResult Error()
+     {
+          return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
      }
 }
